93960f5b11c6275c57c7ef18021afabbced42771
Nicolas Baillot perso <nbaillot.d@gmail.com>
2020-09-18 16:46:56 +0200

    compare_plot_2N + legends + spec vs ritz

--- multi-analysis/multi_analysis.py
+++ multi-analysis/multi_analysis.py
@@ -24,8 +24,8 @@ plt.rcParams.update({"text.usetex": True, "font.size" : 15})
 #results_directory=str(sys.argv[1])
 
 ################################################################################
-def compare_plot(out_name,obj1,obj2,ylabel12,obj3,ylabel3,x,xlabel,io):
-    """Produces usefull comparision plots:
+def compare_plots(out_name,obj1,obj2,ylabel12,obj3,ylabel3,x,xlabel,io):
+    """Produces usefull comparision plots bteween obj1 and obj2:
     Args:
         out_name (string): Name of the output png file.
         obj1, obj2 (list): The two lists of numbers to compare.
@@ -47,7 +47,7 @@ def compare_plot(out_name,obj1,obj2,ylabel12,obj3,ylabel3,x,xlabel,io):
     ax1 = fig.add_subplot(gs[0])
     plt.yscale("log")
     ax1.plot(x[:],obj1,color='blue')
-    ax1.plot(x[:],obj2,color='green')
+    ax1.plot(x[:],obj2,color='blue',linestyle='dashed')
     ymax=max(max(obj1),max(obj2))
     ax1.set_ylabel(ylabel12)
     start, end = ax1.get_xlim()
@@ -69,8 +69,8 @@ def compare_plot(out_name,obj1,obj2,ylabel12,obj3,ylabel3,x,xlabel,io):
 
 
 ################################################################################
-def compare_plot_N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io):
-    """Produces usefull comparision with N plots:
+def compare_plots_2N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io,legend):
+    """Produces usefull comparision with 2N plots:
     Args:
         out_name (string): Name of the output png file.
         obj1 (list)      : List containing lists to be compared together.
@@ -82,21 +82,25 @@ def compare_plot_N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io):
     Return:
         (void): Creates the plot and save it in the png file named by out_name arg.
     """
-    
-    # Create figure window to plot data
+
+    # Create figure window to plot data:
     fig = plt.figure(1, figsize=(9,9))
     gs = gridspec.GridSpec(2, 1, height_ratios=[6, 2])
 
-    # Top plot: [obj11,obj12,..obj1N]
+    # Top plot: [obj11,obj12,..obj1N]:
     ax1 = fig.add_subplot(gs[0])
     plt.yscale("log")
     ymax=0.
-    for obj in obj1:
-        ymax_tmp=max(obj)
+    c=[]
+    for o,obj in enumerate(obj1):
+        c.append(tuple(np.random.random(3)))
+        ymax_tmp=max(max(obj[0]),max(obj[1]))
         if (ymax_tmp>ymax):
-            ymax=ymax_tmp
-        c=np.random.random(3)
-        ax1.plot(x[:],obj,color=c)
+            ymax=ymax_tmp    
+        ax1.plot(x[:],obj[0],color=c[o],label=legend[o][0])
+        ax1.plot(x[:],obj[1],color=c[o],linestyle='dashed',label=legend[o][1])
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+           ncol=2, mode="expand", borderaxespad=0.)
     ax1.set_ylabel(ylabel1)
     start, end = ax1.get_xlim()
     #ax1.xaxis.set_ticks(np.arange(start, end, 1.))
@@ -104,7 +108,8 @@ def compare_plot_N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io):
 
     # Bottom plot: obj2
     ax2 = fig.add_subplot(gs[1])
-    ax2.plot(x[:],obj2,color='black')
+    for o,obj in enumerate(obj2):
+        ax2.plot(x[:],obj,color=c[o])
     ax2.axhline(color="gray", zorder=-1)
     ax2.set_xlabel(xlabel)
     ax2.set_ylabel(ylabel2)
@@ -115,47 +120,40 @@ def compare_plot_N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io):
 ################################################################################
 
 
+
 ################################################################################
 def multi_plot(results_directory,io):
-
+    """Plots J, Jo and Jb for lanczos and PlanczosIf, as well as their difference.
+    """
     # Get the results files from the results directory and store them in lists:
-    lanczos=np.atleast_2d(np.genfromtxt(results_directory+'/lanczos_control_space.dat', comments='#'))
-    PlanczosIF=np.atleast_2d(np.genfromtxt(results_directory+'/PlanczosIF_model_space.dat', comments='#'))
-    diff=np.atleast_2d(np.genfromtxt(results_directory+'/lanczos_control_vs_PlanczosIF_model.dat', comments='#'))
+    lanczos=np.genfromtxt(results_directory+'/lanczos_control_space.dat', comments='#')
+    PlanczosIF=np.genfromtxt(results_directory+'/PlanczosIF_model_space.dat', comments='#')
+    diff=np.genfromtxt(results_directory+'/lanczos_control_vs_PlanczosIF_model.dat', comments='#')
+
+    # lanczos=np.atleast_2d(np.genfromtxt(results_directory+'/lanczos_control_space.dat', comments='#'))
+    # PlanczosIF=np.atleast_2d(np.genfromtxt(results_directory+'/PlanczosIF_model_space.dat', comments='#'))
+    # diff=np.atleast_2d(np.genfromtxt(results_directory+'/lanczos_control_vs_PlanczosIF_model.dat', comments='#'))
 
     # Total iterations (outer x inner):
     itot=list(range(len(lanczos)))
 
     #--------------- Plots for J -----------------------------
     out_name=results_directory+"/lanczos_vs_PlanczosIF_J.png"
-    compare_plot(out_name,lanczos[:,3],PlanczosIF[:,3],r'$J=J_b+J_o$',
+    compare_plots(out_name,lanczos[:,3],PlanczosIF[:,3],r'$J=J_b+J_o$',
                  diff[:,3],r'$\Delta J$',
                  itot,r'iterations',io)
 
     #--------------- Plots for Jb ----------------------------
     out_name=results_directory+"/lanczos_vs_PlanczosIF_Jb.png"
-    compare_plot(out_name, lanczos[:,4],PlanczosIF[:,4],r'$J_b$',
+    compare_plots(out_name, lanczos[:,4],PlanczosIF[:,4],r'$J_b$',
                  diff[:,4],r'$\Delta J_b$',
                  itot,'iterations',io)
 
     #--------------- Plots for Jo ----------------------------
     out_name=results_directory+"/lanczos_vs_PlanczosIF_Jo.png"
-    compare_plot(out_name,lanczos[:,5],PlanczosIF[:,5],r'$J_o$',
+    compare_plots(out_name,lanczos[:,5],PlanczosIF[:,5],r'$J_o$',
                  diff[:,5],r'$\Delta J_o$',
                  itot,'iterations',io)
-
-    # test plot:
-    obj1=[lanczos[:,5],PlanczosIF[:,3],lanczos[:,3]]
-    ylabel1='lala'
-    ylabel2='toto'
-    obj2=diff[:,5]
-    x=itot
-    xlabel='iterations'
-    io=io
-    out_name=results_directory+"/test.png"
-
-    compare_plot_N(out_name,obj1,ylabel1,obj2,ylabel2,x,xlabel,io)
-
-
 ################################################################################
 
+    
--- multi-analysis/run_analysis.py
+++ multi-analysis/run_analysis.py
@@ -13,7 +13,7 @@ import sys
 import numpy as np
 from distutils.dir_util import copy_tree
 from multi_analysis import multi_plot
-from multi_analysis import compare_plot_N
+from multi_analysis import compare_plots_2N
 
 
 # Default values for the parameters:
@@ -45,36 +45,78 @@ os.chdir(cwd)
 if not os.path.exists(results_dir_root):
     os.mkdir(results_dir_root)
 
-# outer iteraions for plotting
+# Outer iteraions for plotting:
 outer_iterations=[]
 for io in range(no):
     outer_iterations.append((ni+1)*io)
 
-# Loop over a given parameter:
-for sigmabvar in [0.0]:
+# Store the results directories produced in the following loop:
+res_dir_list=[]
+
+# Loop over the parameters and run the code:
+for lmp_mode in ['ritz','spectral']:
     
     parameters=[n, no, ni, lmp_mode, full_res, new_seed, sigma_obs, sigmabvar, Lb]
-    print("\n ============== running with sigmabvar={} ============== \n".format(sigmabvar))
     
     # Create the results directory:
     res_dir=results_dir_root+'res_n{}_no{}_ni{}_lmp-{}_sigmao{}_sigmab{}_Lb{}_reso-{}/'.format(n,no,ni,lmp_mode,sigma_obs,sigmabvar,Lb,full_res)
-    
+    res_dir_list.append(res_dir)
     if not os.path.exists(res_dir):
         os.mkdir(res_dir)
-        
+    
     # define the command line to run the code:
     arguments=''
     for par in parameters:
         arguments+=' {} '.format(par)
-        #print(arguments)
     exec_command ='echo '+ arguments + ' | ' + exec_code
-    print(exec_command)
+    print("\n",exec_command,"\n")
     
     # Run the code and save the results:
     os.chdir(path_to_code)
     os.system(exec_command)
     os.chdir(cwd)
     copy_tree(code_output,res_dir)
-    
-    # Run the analysis over the results:
+
+# Run the analysis over the results:
+for res_dir in res_dir_list:
     multi_plot(res_dir,outer_iterations)
+
+
+
+
+# Compare spectral and ritz lmp modes:
+results_directory_test=[]
+labels=[]
+legend=[]
+
+for lmp_mode in ['ritz','spectral']:
+    labels.append(lmp_mode)
+    legend.append([lmp_mode+'-model',lmp_mode+'-control'])
+    results_directory_test.append(results_dir_root+'res_n{}_no{}_ni{}_lmp-{}_sigmao{}_sigmab{}_Lb{}_reso-{}'.format(n,no,ni,lmp_mode,sigma_obs,sigmabvar,Lb,full_res))
+    
+
+diff_list=[]
+obj_list=[]
+
+for res_dir in results_directory_test:
+    res1=np.genfromtxt(res_dir+'/lanczos_control_vs_PlanczosIF_model.dat', comments='#')
+    res2=np.genfromtxt(res_dir+'/PlanczosIF_model_space.dat', comments='#')    
+    res3=np.genfromtxt(res_dir+'/lanczos_control_space.dat', comments='#')
+    diff_list.append(res1[:,3])
+    obj_list.append([res2[:,3],res3[:,3]])
+    
+
+# maybe dirtyish but...
+itot=list(range(len(obj_list[0][0])))
+
+print(legend)
+
+ylabel1=r'$J=J_o+J_b$'
+ylabel2=r'$\Delta J$'
+x=itot
+xlabel='iterations'
+io=outer_iterations
+out_name=results_dir_root+"/test.png"
+
+compare_plots_2N(out_name,obj_list,ylabel1,diff_list,ylabel2,x,xlabel,io,legend)
+
