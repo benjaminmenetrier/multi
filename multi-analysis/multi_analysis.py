01fa6ffd27b0e9fec4f5c4f9fdbf62bc54e98be0
Nicolas Baillot perso <nbaillot.d@gmail.com>
2020-10-02 14:46:29 +0200

    version labo

--- multi-analysis/multi_analysis.py
+++ multi-analysis/multi_analysis.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec 
 import matplotlib.ticker as ticker
 from matplotlib import cm
+from matplotlib.offsetbox import AnchoredText
+
 import numpy as np
 
 
@@ -69,61 +71,6 @@ def compare_plots(out_name,obj1,obj2,ylabel12,obj3,ylabel3,x,xlabel,io):
 ################################################################################
 
 
-
-################################################################################
-def compare_plots_2N(out_name,obj_list,ylabel1,diff_list,ylabel2,x,xlabel,io,legend):
-    """Produces usefull comparision with 2N plots:
-    Args:
-        out_name (string): Name of the output png file.
-        obj_list (list)      : List containing lists to be compared together.
-        yabel (string)   : Label of the y-axis.
-        diff_list (list)      : List of numbers regarding which one wants to compare.
-        ylabel (string)  : Label of diff_list.
-        x (list)         : List of numbers for x-axis.
-        xlabel (string)  : Label of x-axis
-    Return:
-        (void): Creates the plot and save it in the png file named by out_name arg.
-    """
-
-    color_map=cm.get_cmap('copper', len(obj_list))
-    colors=color_map(range(len(obj_list)))
-    # Create figure window to plot data:
-    fig = plt.figure(1, figsize=(9,9))
-    gs = gridspec.GridSpec(2, 1, height_ratios=[6, 2])
-    
-    # Top plot: [obj_list1,obj_list2,..obj_listN]:
-    ax1 = fig.add_subplot(gs[0])
-    plt.yscale("log")
-    ymax=0.
-    for o,obj in enumerate(obj_list):
-        ymax_tmp=max(max(obj[0]),max(obj[1]))
-        if (ymax_tmp>ymax):
-            ymax=ymax_tmp
-        ax1.plot(x[:],obj[0],color=colors[o],label=legend[o][0])
-        ax1.plot(x[:],obj[1],color=colors[o],linestyle='dashed',label=legend[o][1])
-        #ax1.plot(x[:],obj[2],color=c[o],linestyle='dotted',label=legend[o][2])
-        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-           ncol=3, mode="expand", borderaxespad=0.)
-    ax1.set_ylabel(ylabel1)
-    #start, end = ax1.get_xlim()
-    #ax1.xaxis.set_ticks(np.arange(start, end, 1.))
-    ax1.vlines(io, 0, ymax, colors='black', linestyles='dashed')
-
-    # Bottom plot: diff_list
-    ax2 = fig.add_subplot(gs[1])
-    for d,diff in enumerate(diff_list):
-        ax2.plot(x[:],diff,color=colors[d])
-    ax2.axhline(color="gray", zorder=-1)
-    ax2.set_xlabel(xlabel)
-    ax2.set_ylabel(ylabel2)
-    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-    #ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.4f'))
-    plt.savefig(out_name)
-    plt.clf()
-################################################################################
-
-
-
 ################################################################################
 def evolution_plot(results_directory,io):
     """Plots J, Jo and Jb for lanczos and PlanczosIf, as well as their difference.
@@ -175,14 +122,69 @@ def multi_plot(res_dir_list,outer_iterations):
 
 
 ################################################################################
+def compare_plots_2N(out_name,obj_list,ylabel1,diff_list,ylabel2,x,xlabel,io,legend):
+    """Produces usefull comparision with 2N plots:
+    Args:
+        out_name (string): Name of the output png file.
+        obj_list (list)      : List containing lists to be compared together.
+        yabel (string)   : Label of the y-axis.
+        diff_list (list)      : List of numbers regarding which one wants to compare.
+        ylabel (string)  : Label of diff_list.
+        x (list)         : List of numbers for x-axis.
+        xlabel (string)  : Label of x-axis
+    Return:
+        (void): Creates the plot and save it in the png file named by out_name arg.
+    """
+
+    color_map=cm.get_cmap('copper', len(obj_list))
+    colors=color_map(range(len(obj_list)))
+    # Create figure window to plot data:
+    fig = plt.figure(1, figsize=(9,9))
+    gs = gridspec.GridSpec(2, 1, height_ratios=[6, 2])
+
+    print(out_name)
+    # Top plot: [obj_list1,obj_list2,..obj_listN]:
+    ax1 = fig.add_subplot(gs[0])
+    at = AnchoredText("Figure 1a",
+                  prop=dict(size=15), frameon=True,loc='upper left',)
+    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.1")
+    ax1.add_artist(at)
+    plt.yscale("log")
+    ymax=0.
+    for o,obj in enumerate(obj_list):
+        ymax_tmp=max(max(obj[0]),max(obj[1]))
+        if (ymax_tmp>ymax):
+            ymax=ymax_tmp
+        ax1.plot(x[:],obj[0],color=colors[o],label=legend[o][0])
+        ax1.plot(x[:],obj[1],color=colors[o],linestyle='dashed',label=legend[o][1])
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+           ncol=3, mode="expand", borderaxespad=0.)
+    ax1.set_ylabel(ylabel1)
+    #start, end = ax1.get_xlim()
+    #ax1.xaxis.set_ticks(np.arange(start, end, 1.))
+    ax1.vlines(io, 0, ymax, colors='black', linestyles='dashed')
+
+    # Bottom plot: diff_list
+    ax2 = fig.add_subplot(gs[1])
+    for d,diff in enumerate(diff_list):
+        ax2.plot(x[:],diff,color=colors[d])
+    ax2.axhline(color="gray", zorder=-1)
+    ax2.set_xlabel(xlabel)
+    ax2.set_ylabel(ylabel2)
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+    #ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.4f'))
+    plt.savefig(out_name)
+    plt.clf()
+################################################################################
+
+
+################################################################################
 def lmp_compare(out_names,lmp_to_compare,outer_iterations_list):
     """Compare spectral and ritz lmp modes:
     """
 
-    labels=[]
     legend=[]
     for lmp_mode in ['ritz','spectral','none']:
-        labels.append(lmp_mode)
         legend.append([lmp_mode+'-model',lmp_mode+'-control'])
         
 
@@ -200,7 +202,7 @@ def lmp_compare(out_names,lmp_to_compare,outer_iterations_list):
         itot=list(range(len(res1)))
         print("plotting lmp comparision for:\n",out_names[r])
         ylabel1=r'$J=J_o+J_b$'
-        ylabel2=r'$\Delta J$'
+        ylabel2=r'$J_{B^{1/2}}-J_{B}$'
         x=itot
         xlabel='iterations'
         out_name=out_names[r]
@@ -208,23 +210,25 @@ def lmp_compare(out_names,lmp_to_compare,outer_iterations_list):
         try:
             compare_plots_2N(out_name,obj_list,ylabel1,diff_list,ylabel2,x,xlabel,outer_iterations,legend)
         except:
+            #compare_plots_2N(out_name,obj_list,ylabel1,diff_list,ylabel2,x,xlabel,outer_iterations,legend)
             print("Error with lmp comparision of:",res_dirs)
 ################################################################################
+
 
 ################################################################################
 def diff_plot(out_names,param_list,res_dir_list):
 
-    labels=[]
     legend=[]
     for lmp_mode in ['ritz','spectral','none']:
-        labels.append(lmp_mode)
-        legend.append([lmp_mode+'-model',lmp_mode+'-control'])    
-    ylabel=r'\delta J'
+            for par in param_list:
+                legend.append([lmp_mode+'-model',lmp_mode+'-control'])
+
+    ylabel=r'$J_{B^{1/2}}-J_{B}$'
     xlabel=r'\N_obs'
     color_map=cm.get_cmap('copper', 3)
-    colors=color_map(range(3))
+    colors=color_map(range(len(param_list)))
 
-    for r,res_dir in enumerate(res_dir_list):
+    for r,res_dirs in enumerate(res_dir_list):
         diff_list=[]
         for res_dir in res_dirs:
             res=np.genfromtxt(res_dir+'lanczos_control_vs_PlanczosIF_model.dat', comments='#')
@@ -232,10 +236,14 @@ def diff_plot(out_names,param_list,res_dir_list):
             
     # Create figure window to plot data:
     fig = plt.figure(1, figsize=(9,9))
-    
-    # Top plot: [obj_list1,obj_list2,..obj_listN]:
-    ax1.plot(param_list[:],diff_list[:],color=colors[o],label=legend[o][0])
+
+    for par in param_list: 
+        ax1.plot(param_list[:],diff_list[:],color=colors[o],label=legend[o][0])
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-                               ncol=3, mode="expand", borderaxespad=0.)
+                                   ncol=3, mode="expand", borderaxespad=0.)
     ax1.set_ylabel(ylabel)
     ax1.set_xlabel(xlabel)
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+    plt.savefig(out_name)
+    plt.clf()
+################################################################################
--- multi-analysis/run_analysis.py
+++ multi-analysis/run_analysis.py
@@ -14,15 +14,16 @@ import numpy as np
 from distutils.dir_util import copy_tree
 from multi_analysis import multi_plot
 from multi_analysis import lmp_compare
+from multi_analysis import diff_plot
 from fnmatch import fnmatch
 
 # Default values for the parameters:
-n=128
+nobs=128
 no=4 # error when no>4 ?
 ni=5
 lmp_mode='ritz'
-full_res='F'
-new_seed='T'
+full_res='T'
+new_seed='F'
 sigma_obs=0.1
 sigmabvar=0.0
 Lb=0.005
@@ -35,26 +36,33 @@ path_to_code='..'
 exec_code='./run/multi '
 
 # Output paths:
+out_dirs=[]
+
 # Where are written the raw outputs of the code:
 code_output='../results'
-if not os.path.exists(code_output):
-    os.mkdir(code_output)
+out_dirs.append(code_output)
 
 # Global results of the analysis:
 results_dir_root='./analysis_results/'
-if not os.path.exists(results_dir_root):
-    os.mkdir(results_dir_root)
+out_dirs.append(results_dir_root)
 
 # Raw results of the analysis: 
 res_dir_raw=results_dir_root+'raw_results/'
-if not os.path.exists(res_dir_raw):
-    os.mkdir(res_dir_raw)
+out_dirs.append(res_dir_raw)
 
 # Results for the comparision between LMP modes:
 res_dir_lmp_compare=results_dir_root+'lmp_compare/'
-if not os.path.exists(res_dir_lmp_compare):
-    os.mkdir(res_dir_lmp_compare)
+out_dirs.append(res_dir_lmp_compare)
+    
+# Results for the evolution of the difference in J vs nobs
+res_dir_diff_vs_nobs=results_dir_root+'diff_vs_nobs/'
+out_dirs.append(res_dir_diff_vs_nobs)
 
+# Creates the results directories:
+for dir in out_dirs:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    
 # Get the current working directory:
 cwd=os.getcwd()
 
@@ -74,12 +82,12 @@ outer_iterations_list=[]
 
 # Loop over the parameters and run the code:
 for lmp_mode in ['ritz','spectral','none']:
-    for n in [128,2048]:
-        for no in [3,5]:
-            for ni in [4,8]:
-                for sigma_obs in [0.1,0.2]:
-                    for sigmabvar in [0.,0.01]:
-                        for Lb in [0.005,0.1]:
+    for nobs in [128,2048]:
+        for no in [3]:
+            for ni in [4]:
+                for sigma_obs in [0.1]:
+                    for sigmabvar in [0]:
+                        for Lb in [0.005]:
                             # Outer iteraions for plotting:
                             outer_iterations=[]
                             for io in range(no):
@@ -87,10 +95,10 @@ for lmp_mode in ['ritz','spectral','none']:
                             outer_iterations_list.append(outer_iterations)
 
                             # parameters of the code:
-                            parameters=[n, no, ni, lmp_mode, full_res, new_seed, sigma_obs, sigmabvar, Lb]
+                            parameters=[nobs, no, ni, lmp_mode, full_res, new_seed, sigma_obs, sigmabvar, Lb]
 
                             # Create the results directory:
-                            res_dir=res_dir_raw+'res_n{}_no{}_ni{}_lmp-{}_sigmao{}_sigmab{}_Lb{}_reso-{}/'.format(n,no,ni,lmp_mode,sigma_obs,sigmabvar,Lb,full_res)
+                            res_dir=res_dir_raw+'res_nobs{}_no{}_ni{}_lmp-{}_sigmao{}_sigmab{}_Lb{}_reso-{}/'.format(nobs,no,ni,lmp_mode,sigma_obs,sigmabvar,Lb,full_res)
                             res_dir_list.append(res_dir)
 
                             if not rewrite_results and os.path.exists(res_dir):
@@ -116,7 +124,7 @@ for lmp_mode in ['ritz','spectral','none']:
 
 ################################################################################
 # Plot the results of the code:
-multi_plot(res_dir_list,outer_iterations_list)
+#multi_plot(res_dir_list,outer_iterations_list)
 ################################################################################
 
 
@@ -138,9 +146,7 @@ for r,res_dir in enumerate(res_dir_list):
         # Store the output files names
         out_name=res_tmp1+'compare'+res_tmp2
         out_name=out_name.split(res_dir_raw)[1]
-        if not os.path.exists(res_dir_lmp_compare+out_name):
-            os.mkdir(res_dir_lmp_compare+out_name)
-        out_name=res_dir_lmp_compare+out_name+'lmp_compare.png'
+        out_name=res_dir_lmp_compare+out_name[:-1]+'.png'
         out_names.append(out_name)
         # Store the outer iterations:
         outer_iterations_list_tmp.append(outer_iterations_list[r])
@@ -155,8 +161,8 @@ lmp_compare(out_names,lmp_to_compare,outer_iterations_list)
 ################################################################################
 
 
-
-
+################################################################################
+# Comparision of LMP methods:
 
 # Output filenames:
 out_names=[]
@@ -165,16 +171,14 @@ outer_iterations_list_tmp=[]
 # Store the results files to compare:
 lmp_to_compare=[]
 
-for n in []
+for r,res_dir in enumerate(res_dir_list):
     if 'ritz' in res_dir:
         res_tmp=res_dir.split('ritz')
         res_tmp1,res_tmp2=res_tmp[0],res_tmp[1]
         # Store the output files names
         out_name=res_tmp1+'compare'+res_tmp2
         out_name=out_name.split(res_dir_raw)[1]
-        if not os.path.exists(res_dir_lmp_compare+out_name):
-            os.mkdir(res_dir_lmp_compare+out_name)
-        out_name=res_dir_lmp_compare+out_name+'lmp_compare.png'
+        out_name=res_dir_lmp_compare+out_name[:-1]+'.png'
         out_names.append(out_name)
         # Store the outer iterations:
         outer_iterations_list_tmp.append(outer_iterations_list[r])
@@ -183,3 +187,38 @@ for n in []
         for lmp in ['ritz','spectral','none']:
             lmp_to_compare_tmp.append(res_tmp1+lmp+res_tmp2)
         lmp_to_compare.append(lmp_to_compare_tmp)
+
+# Plots the comparision of LMP methods:
+lmp_compare(out_names,lmp_to_compare,outer_iterations_list)
+################################################################################
+
+
+################################################################################
+# Output filenames:
+out_names=[]
+# Store the outer_itertaions:
+outer_iterations_list_tmp=[]
+# Store the results files to compare:
+diff_vs_nobs=[]
+
+for r,res_dir in enumerate(res_dir_list):
+        res_tmp=res_dir.split('nobs')
+        res_tmp1,res_tmp2=res_tmp[0],res_tmp[1]
+        # Store the output files names
+        out_name=res_tmp1+'compare'+res_tmp2
+        out_name=out_name.split(res_dir_raw)[1]
+        out_name=res_dir_diff_vs_nobs+out_name[:-1]+'.png'
+        out_names.append(out_name)
+        # Store the outer iterations:
+        outer_iterations_list_tmp.append(outer_iterations_list[r])
+        # Store the results files to compare:
+        diff_vs_nobs_tmp=[]
+        for nobs in [128,2048]:
+            diff_vs_nobs_tmp.append(res_tmp1+str(nobs)+res_tmp2)
+        diff_vs_nobs.append(diff_vs_nobs_tmp)
+        print(diff_vs_nobs)
+# Plots the comparision of LMP methods:
+diff_plot(out_names,diff_vs_nobs,outer_iterations_list)        
+################################################################################
+
+
